e807a5e53ea14fc56d2e716b2d1223c1ed791533
Eugene Gladchenko <gladchenko@gmail.com>
2020-06-29 08:41:21 +0000

    cd to the directory of the script to run safely with cron

--- cert_renew.sh
+++ cert_renew.sh
@@ -3,5 +3,7 @@
 COMPOSE="/usr/local/bin/docker-compose --no-ansi"
 DOCKER="/usr/bin/docker"
 
+cd "$(dirname "${BASH_SOURCE[0]}")"
+
 $COMPOSE run zabbix-certbot && $COMPOSE kill -s HUP zabbix-nginx
 #$DOCKER system prune -af
